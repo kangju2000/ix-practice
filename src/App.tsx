87e36996538f08486e53bc3ba4b5ef273688e670
kangju2000 <kangju2000@naver.com>
2023-12-10 02:27:21 +0900

    framer-motion, gsap 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
 import { css } from '@emotion/react';
+import gsap from 'gsap';
+import { useLayoutEffect, useRef } from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+import { motion } from 'framer-motion';
 
 function App() {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    gsap.to(ref.current, {
+      duration: 2,
+      x: 200,
+      rotation: 360,
+    });
+  });
+
   return (
-    <div
-      css={css`
-        background-color: red;
-      `}
-    >
-      하이
+    <div>
+      <GlobalStyle />
+      <h1>테스트</h1>
+      <div
+        ref={ref}
+        css={css`
+          width: 100px;
+          height: 100px;
+          background-color: blue;
+          border-radius: 8px;
+        `}
+      ></div>
+      <div
+        css={css`
+          height: 100px;
+        `}
+      ></div>
+      <motion.div
+        css={css`
+          width: 100px;
+          height: 100px;
+          background-color: blue;
+          border-radius: 8px;
+        `}
+        animate={{
+          x: 200,
+          rotate: 360,
+        }}
+        transition={{
+          duration: 2,
+        }}
+      ></motion.div>
     </div>
   );
 }
